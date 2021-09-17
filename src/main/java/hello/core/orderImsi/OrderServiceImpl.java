f2d03ea63a2781709450a627e23e5559370b174a
junyharang <junyharang8592@gmail.com>
2021-09-17 20:24:45 +0900

    210917 : 의존관계 주입을 위한 Anntation 만들기 공부 완료

--- src/main/java/hello/core/orderImsi/OrderServiceImpl.java
+++ src/main/java/hello/core/orderImsi/OrderServiceImpl.java
@@ -1,11 +1,10 @@
 package hello.core.orderImsi;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 // @RequiredArgsConstructor는 final이 붙은 필드를 대상으로 생성자를 만들어준다.
@@ -23,7 +22,7 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     // 생성자를 통해 DI가 가능하게 하면 NPE를 막을 수 있다.
-      @Autowired public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+      @Autowired public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
           this.memberRepository = memberRepository;
           this.discountPolicy = discountPolicy;
       }
